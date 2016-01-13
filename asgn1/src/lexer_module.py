bb9ed060cf217f88d3e15df2da655108ba78f8f1
Sundararajan R <dyoucme@gmail.com>
2016-01-13 15:25:58 +0530

    Added token rules for operators
    Except for Stringwise Equality, Logical and Quote-like operators.
    We need to add keywords for some of those.

--- asgn1/src/lexer_module.py
+++ asgn1/src/lexer_module.py
@@ -9,19 +9,71 @@ class Lexer(object): # Inheriting from object provides extra functionality
     # List of token names.   This is always required
     tokens = (
        'NUMBER',
-       'PLUS',
-       'MINUS',
-       'TIMES',
-       'DIVIDE',
+
+       'PLUS', 'MINUS', 'TIMES', 'DIVIDE', 'MODULUS', 'EXPONENT',
+       'OR', 'AND', 'NOT', 'XOR', 'LSHIFT', 'RSHIFT',
+       'LT', 'GT', 'LE', 'GE', 'EQ', 'NE', 'CMP',
+
+       # Assignment Operators
+       'EQUALS', 'TIMESEQUAL', 'DIVEQUAL', 'MODEQUAL',
+       'PLUSEQUAL', 'MINUSEQUAL',
+       'LSHIFTEQUAL','RSHIFTEQUAL', 'ANDEQUAL', 'XOREQUAL',
+       'OREQUAL', 'EXPEQUAL',
+
+       # Miscellaneous Operators
+       'DOT', 'REPEAT', 'RANGE', 'INC', 'DEC', 'ARROW',
+
        'LPAREN',
        'RPAREN',
     )
 
-    # Regular expression rules for simple tokens
+    # Arithmetic Operators
     t_PLUS    = r'\+'
     t_MINUS   = r'-'
     t_TIMES   = r'\*'
     t_DIVIDE  = r'/'
+    t_MODULUS = r'%'
+    t_EXPONENT = r'\*\*'
+
+    # Bitwise Operators
+    t_OR = r'\|'
+    t_AND = r'&'
+    t_NOT = r'~'
+    t_XOR = r'\^'
+    t_LSHIFT = r'<<'
+    t_RSHIFT = r'>>'
+
+    # Equality Operators
+    t_LT  = r'<'
+    t_GT  = r'>'
+    t_LE  = r'<='
+    t_GE  = r'>='
+    t_EQ  = r'=='
+    t_NE  = r'!='
+    t_CMP = r'<=>'
+
+    # Assignment Operators
+    t_EQUALS      = r'='
+    t_PLUSEQUAL   = r'\+='
+    t_MINUSEQUAL  = r'-='
+    t_TIMESEQUAL  = r'\*='
+    t_DIVEQUAL    = r'/='
+    t_MODEQUAL    = r'%='
+    t_EXPEQUAL    = r'\*\*='
+    t_LSHIFTEQUAL = r'<<='
+    t_RSHIFTEQUAL = r'>>='
+    t_ANDEQUAL    = r'&='
+    t_XOREQUAL    = r'\^='
+    t_OREQUAL     = r'\|='
+
+    # Miscellaneous Operators
+    t_DOT = r'\.'
+    t_REPEAT = r'x'	    ## Warning: Clashes with ID??
+    t_RANGE = r'\.\.'
+    t_INC = r'\+\+'
+    t_DEC = r'--'
+    t_ARROW = r'->'
+
     t_LPAREN  = r'\('
     t_RPAREN  = r'\)'
 
