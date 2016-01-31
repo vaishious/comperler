50f7e8856f251d678f5fc39fe4862dfa3a3762b7
Pranav Vaish <pranav.vaish@gmail.com>
2016-02-01 01:46:37 +0530

    Improved and added StrCmp function

--- asgn2/lib/hashlib.c
+++ asgn2/lib/hashlib.c
@@ -40,14 +40,14 @@ void *alloc(unsigned int size)
 int strCmp(char *s1,char *s2)
 {
 	int i;
-	for(i = 0;s1[i] && s2[i];i++) {
-		if(s1[i] != s2[i]) {
+	for(i = 0;s1[i] || s2[i];i++) {				// Only end if both strings terminate
+		if(s1[i] > s2[i]) {						// If the next character of any one string is more in value than that of another then return 1, -1 appropriately
+			return 1;
+		} else if(s1[i] < s2[i]) {
 			return -1;
 		}
 	}
-	if(s1[i] || s2[i])		// String lengths differ
-		return -1;
-	return 0;
+	return 0;									// Will only reach here if both have same length and same characters at all points
 }
 
 Element *findMatch(Hash *hashPtr,char *s)
