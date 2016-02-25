fd4fa0ae4f590d82b8651240909eabee87ca77ad
Sundararajan R <dyoucme@gmail.com>
2016-02-25 11:27:04 +0530

    Added rules for expressions and branch statements
    Haven't yet set the actions to be taken on a reduce for any rule.

--- asgn3/src/parser_module.py
+++ asgn3/src/parser_module.py
@@ -2,10 +2,123 @@ import ply.yacc as yacc
 
 class Parser(object):
 
+    precedence = (
+        ('left', 'OR'),
+        ('left', 'AND'),
+        ('left', 'BOR', 'BXOR'),
+        ('left', 'BAND'),
+        ('nonassoc', 'EQ', 'NE', 'CMP', 'STREQ', 'STRNE', 'STRCMP'),
+        ('nonassoc', 'LT', 'GT', 'LE', 'GE', 'STRLT', 'STRGT', 'STRLE', 'STRGE'),
+        ('left', 'LSHIFT', 'RSHIFT'),
+        ('left', 'PLUS', 'MINUS'),
+        ('left', 'TIMES','DIVIDE', 'MODULUS'),
+        ('right', 'EXPONENT'),
+    )
+
+    def p_statements(self, p):
+        ''' statements : statement statements
+                       | statement
+        '''
+
+    def p_codeblock(self, p):
+        ''' codeblock : LBLOCK statements RBLOCK '''
+
+    def p_empty(self, p):
+        ''' empty : '''
+
+    def p_statement(self, p):
+        ''' statement : assignment 
+                      | branch 
+        '''
+
     # Dummy rule for now
     def p_assignment(self, p):
-        'assignment : VARIABLE EQUALS NUMBER SEMICOLON'
-        print "%s %s %d%s"%(p[1], p[2], p[3], p[4])
+        ''' assignment : VARIABLE EQUALS expression SEMICOLON '''
+
+    def p_expression(self, p):
+        ''' expression : expression PLUS expression
+                       | expression MINUS expression
+                       | expression TIMES expression
+                       | expression DIVIDE expression
+                       | expression MODULUS expression
+                       | expression EXPONENT expression
+                       | expression BOR expression
+                       | expression BAND expression
+                       | expression BXOR expression
+                       | expression LSHIFT expression
+                       | expression RSHIFT expression
+                       | LPAREN expression RPAREN
+                       | function-call
+                       | var
+                       | const
+        '''
+
+    def p_condition(self, p):
+        ''' condition : condition AND condition
+                      | condition OR condition
+                      | expression LT expression
+                      | expression GT expression
+                      | expression LE expression
+                      | expression GE expression
+                      | expression EQ expression
+                      | expression NE expression
+                      | expression CMP expression
+                      | expression STRLT expression
+                      | expression STRGT expression
+                      | expression STRLE expression
+                      | expression STRGE expression
+                      | expression STREQ expression
+                      | expression STRNE expression
+                      | expression STRCMP expression
+        '''
+
+    def p_branch(self, p):
+        ''' branch : if-elsif-else
+                   | unless
+        '''
+
+    def p_if_elsif_else(self, p):
+        ''' if-elsif-else : if-elsif else '''
+
+    def p_if_elsif(self, p):
+        ''' if-elsif : IF LPAREN condition RPAREN codeblock elsif '''
+
+    def p_elsif(self, p):
+        ''' elsif : ELSIF LPAREN condition RPAREN codeblock elsif
+                  | empty
+        '''
+
+    def p_else(self, p):
+        ''' else : ELSE codeblock
+                 | empty
+        '''
+
+    def p_unless(self, p):
+        ''' unless : UNLESS LPAREN condition RPAREN codeblock else '''
+
+    # Need to handle many other cases
+    def p_variable(self, p):
+        ''' var : VARIABLE
+                | REFERENCE
+                | DEREFERENCE
+        '''
+
+    # Need to handle many other cases
+    def p_constant(self, p):
+        ''' const : numeric
+                  | SINGQUOTSTR
+                  | DOUBQUOTSTR
+        '''
+
+    def p_numeric(self, p):
+        ''' numeric : NUMBER
+                    | BINARY
+                    | OCTAL
+                    | HEXADECIMAL
+        '''
+
+    def p_function_call(self, p):
+        ''' function-call : ID LPAREN expression RPAREN '''
 
     # Error rule for syntax errors
     def p_error(self, p):
--- asgn3/test/test1.pl
+++ asgn3/test/test1.pl
@@ -1,1 +1,18 @@
-$abc = 34;
+$abc = 2+3;
+$pqr = 3**4;
+if ($abc < $pqr) {
+	$mno = 3;
+}
+elsif ($abc > $pqr) {
+	$mno = 4;
+}
+else {
+	$mno = 5;
+}
+
+unless ($abc == $pqr) {
+	$mno = 6;
+}
+else {
+	$mno = 7;
+}
