9a28b36c9bff66ea8434f3014a06a83a9ac31ecc
Rbavishi <rohan.bavishi95@gmail.com>
2016-04-04 19:32:00 +0530

    Final tests added

--- asgn4/test/test6.pl
+++ asgn4/test/test6.pl
@@ -2,7 +2,3 @@
 
 my %data;
 %data = ('John Paul' => 45, 'Lisa' => 30, 'Kumar' => 40);
-
-#print "\$data{'John Paul'} = $data{'John Paul'}\n";
-#print "\$data{'Lisa'} = $data{'Lisa'}\n";
-#print "\$data{'Kumar'} = $data{'Kumar'}\n";
