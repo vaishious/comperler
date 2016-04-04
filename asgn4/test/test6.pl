4973d0ba341daa28fbdf9a56e854264644d673c0
Pranav Vaish <pranav.vaish@gmail.com>
2016-04-04 18:58:05 +0530

    Simple Array and Hash tests

---
+++ asgn4/test/test6.pl
@@ -0,0 +1,8 @@
+#!/usr/bin/perl
+
+my %data;
+%data = ('John Paul' => 45, 'Lisa' => 30, 'Kumar' => 40);
+
+#print "\$data{'John Paul'} = $data{'John Paul'}\n";
+#print "\$data{'Lisa'} = $data{'Lisa'}\n";
+#print "\$data{'Kumar'} = $data{'Kumar'}\n";
