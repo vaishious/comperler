fa1ff9f07f54cd93e8119b5ecc2767dc1bf3697c
Pranav Vaish <pranav.vaish@gmail.com>
2016-04-12 04:52:28 +0530

    Syntax Error handling improved

--- project/src/irgen/parser_module.py
+++ project/src/irgen/parser_module.py
@@ -89,6 +89,7 @@ class Parser(object):
     def p_mark_check_declaration(self, p):
         ''' MARK-check-declaration : '''
 
+        # TODO: Should this be the same?
         if not p[-1].symEntry.CheckDeclaration():
             raise DEBUG.PerlNameError("Variable %s not defined"%(p[-1].symEntry.baseVarName))
 
@@ -220,6 +221,11 @@ class Parser(object):
         else:
             p[0].code = IR.ListIR()
 
+    def p_list_elements_error(self, p):
+        ''' list-elements : error COMMA arith-bool-string-expression
+        '''
+        self.error_list.append((p.lineno(2), "Invalid list elements at line %d"%(p.lineno(2))))       
+
     def p_hash_expression(self, p):
         ''' hash-expression : LPAREN hash-elements RPAREN '''
 
@@ -247,6 +253,11 @@ class Parser(object):
             p[0].typePlace = p[1].typePlace
             p[0].code = p[1].code | p[3].code | p[5].code | IR.GenCode("typeassign, %s{%s}, %s"%(p[1].place, p[3].place, p[5].typePlace)) | IR.GenCode("=, %s{%s}, %s"%(p[1].place, p[3].place, p[5].place))
 
+    def p_hash_elements_error(self, p):
+        ''' hash-elements : error COMMA arith-bool-string-expression HASHARROW arith-bool-string-expression
+                          | error HASHARROW arith-bool-string-expression
+        '''
+
     def p_arith_bool_string_expression(self, p):
         ''' arith-bool-string-expression : arith-boolean-expression
                                          | string-expression
@@ -264,6 +275,11 @@ class Parser(object):
 
             IR.BackPatch(p[1].falselist, IR.NextInstr)
             p[0].code = p[0].code | IR.GenCode("typeassign, %s, %d"%(p[0].typePlace, TYPE_INT)) | IR.GenCode("=, %s, 0"%(p[0].place))
+
+    def p_arith_bool_string_expression_error(self, p):
+        ''' arith-bool-string-expression : LPAREN error RPAREN
+        '''
+        self.error_list.append((p.lineno(3), "Invalid Expression starting at line %d and ending at line %d"%(p.lineno(1),p.lineno(3))))       
 
     def p_arith_expression(self, p):
         ''' arith-expression : numeric
@@ -705,7 +721,8 @@ class Parser(object):
         p[0].falselist = p[9].falselist
 
     def p_if_elsif_error(self, p):
-        ''' if-elsif : IF LPAREN error RPAREN codeblock elsif '''
+        ''' if-elsif : IF LPAREN error RPAREN MARK-backpatch codeblock
+                     | IF LPAREN error RPAREN MARK-backpatch codeblock MARK-backpatch-nextlist MARK-backpatch elsif '''
         self.error_list.append((p.lineno(3), "Line %d: Invalid conditional passed to IF"%(p.lineno(3))))
 
     def p_elsif1(self, p):
@@ -775,7 +792,8 @@ class Parser(object):
         p[0].falselist = p[3].truelist
 
     def p_unless_error(self, p):
-        ''' unless-elsif : UNLESS LPAREN error RPAREN codeblock elsif ELSE codeblock '''
+        ''' unless-elsif : UNLESS LPAREN error RPAREN MARK-backpatch codeblock MARK-backpatch-nextlist MARK-backpatch elsif
+                         | UNLESS LPAREN error RPAREN MARK-backpatch codeblock '''
         self.error_list.append((p.lineno(3), "Line %d: Invalid conditional passed to UNLESS"%(p.lineno(3))))
 
     def p_continue_block(self, p):
@@ -817,6 +835,10 @@ class Parser(object):
 
         p[0].code = p[4].code | p[7].code | IR.GenCode("goto, %d"%(p[2].instr))
 
+    def p_while_loop_error(self, p):
+        ''' while-loop : WHILE MARK-backpatch LPAREN error RPAREN MARK-backpatch codeblock '''
+        self.error_list.append((p.lineno(4), "Line %d: Invalid conditional passed to WHILE loop"%(p.lineno(4))))
+
     def p_while_loop_continue(self, p):
         ''' while-loop : WHILE MARK-backpatch LPAREN boolean-expression RPAREN MARK-backpatch codeblock MARK-backpatch continue '''
 
@@ -856,6 +878,9 @@ class Parser(object):
 
         p[0].code = p[4].code | p[7].code | p[9].code | IR.GenCode("goto, %d"%(p[2].instr))
 
+    def p_while_loop_continue_error(self, p):
+        ''' while-loop : WHILE MARK-backpatch LPAREN error RPAREN MARK-backpatch codeblock MARK-backpatch continue '''
+
     def p_until_loop(self, p):
         ''' until-loop : UNTIL MARK-backpatch LPAREN boolean-expression RPAREN MARK-backpatch codeblock '''
 
@@ -888,6 +913,10 @@ class Parser(object):
         p[0].loop_last_list.pop(defaultID, None)
 
         p[0].code = p[4].code | p[7].code | IR.GenCode("goto, %d"%(p[2].instr))
+
+    def p_until_loop_error(self, p):
+        ''' until-loop : UNTIL MARK-backpatch LPAREN error RPAREN MARK-backpatch codeblock '''
+        self.error_list.append((p.lineno(4), "Line %d: Invalid conditional passed to UNTIL loop"%(p.lineno(4))))
 
     def p_until_loop_continue(self, p):
         ''' until-loop : UNTIL MARK-backpatch LPAREN boolean-expression RPAREN MARK-backpatch codeblock MARK-backpatch continue '''
@@ -928,6 +957,10 @@ class Parser(object):
 
         p[0].code = p[4].code | p[7].code | p[9].code | IR.GenCode("goto, %d"%(p[2].instr))
 
+    def p_until_loop_continue_error(self, p):
+        ''' until-loop : UNTIL MARK-backpatch LPAREN error RPAREN MARK-backpatch codeblock MARK-backpatch continue '''
+        self.error_list.append((p.lineno(4), "Line %d: Invalid conditional passed to UNTIL loop"%(p.lineno(4))))
+
     def p_do_while_loop(self, p):
         # Next, redo and last give a compilation error when used with do-while
         ''' do-while-loop : DO MARK-backpatch codeblock MARK-backpatch WHILE LPAREN boolean-expression RPAREN SEMICOLON '''
@@ -940,6 +973,11 @@ class Parser(object):
         p[0].CopyLoopLists(p[3])
 
         p[0].code = p[3].code | p[7].code
+
+    def p_do_while_loop_error(self, p):
+        # Next, redo and last give a compilation error when used with do-while
+        ''' do-while-loop : DO MARK-backpatch codeblock MARK-backpatch WHILE LPAREN error RPAREN SEMICOLON '''
+        self.error_list.append((p.lineno(7), "Line %d: Invalid conditional passed to DO WHILE loop"%(p.lineno(7))))
 
     def p_foreach_loop(self, p):
         ''' foreach-loop : FOREACH var-lhs LPAREN var-lhs RPAREN MARK-backpatch codeblock '''
@@ -976,6 +1014,14 @@ class Parser(object):
         p[0].loop_last_list.pop(defaultID, None)
 
         p[0].code = p[0].code | p[7].code | IR.GenCode("=, +, %s, %s, 1"%(tempIterator, tempIterator)) | IR.GenCode("goto, %d"%(instrBoolean))
+
+    def p_foreach_loop_error1(self, p):
+        ''' foreach-loop : FOREACH error LPAREN var-lhs RPAREN MARK-backpatch codeblock '''
+        self.error_list.append((p.lineno(2), "Line %d: Invalid variable name in to FOREACH loop"%(p.lineno(2))))
+
+    def p_foreach_loop_error2(self, p):
+        ''' foreach-loop : FOREACH var-lhs LPAREN error RPAREN MARK-backpatch codeblock '''
+        self.error_list.append((p.lineno(4), "Line %d: Invalid list given to the FOREACH loop"%(p.lineno(4))))
 
     def p_foreach_loop_continue(self, p):
         ''' foreach-loop : FOREACH var-lhs LPAREN var-lhs RPAREN MARK-backpatch codeblock MARK-backpatch continue '''
@@ -1022,6 +1068,14 @@ class Parser(object):
 
         p[0].code = p[0].code | p[7].code | p[9].code | IR.GenCode("=, +, %s, %s, 1"%(tempIterator, tempIterator)) | IR.GenCode("goto, %d"%(instrBoolean))
 
+    def p_foreach_loop_continue_error1(self, p):
+        ''' foreach-loop : FOREACH error LPAREN var-lhs RPAREN MARK-backpatch codeblock MARK-backpatch continue '''
+        self.error_list.append((p.lineno(2), "Line %d: Invalid variable name in to FOREACH loop"%(p.lineno(2))))
+
+    def p_foreach_loop_continue_error2(self, p):
+        ''' foreach-loop : FOREACH var-lhs LPAREN error RPAREN MARK-backpatch codeblock MARK-backpatch continue '''
+        self.error_list.append((p.lineno(4), "Line %d: Invalid list given to the FOREACH loop"%(p.lineno(4))))
+
     def p_for_loop(self, p): # We don't allow declarations in the for-loop specification
         ''' for-loop : FOR LPAREN expression SEMICOLON MARK-backpatch boolean-expression SEMICOLON MARK-backpatch expression MARK-backpatch-nextlist RPAREN MARK-backpatch codeblock '''
 
@@ -1062,30 +1116,6 @@ class Parser(object):
         '''
 
         p[0] = p[1]
-
-    # Fix these error messages
-    """
-    def p_loop_error_a(self, p):
-        ''' loop : UNTIL LPAREN error RPAREN codeblock
-                 | FOR LPAREN error RPAREN codeblock
-        '''
-        if p[1] == 'while':
-            self.error_list.append((p.lineno(3), "Line %d: Invalid conditional passed to WHILE loop"%(p.lineno(3))))
-        elif p[1] == 'until':
-            self.error_list.append((p.lineno(3), "Line %d: Invalid conditional passed to UNTIL loop"%(p.lineno(3))))
-        elif p[1] == 'for':
-            self.error_list.append((p.lineno(3), "Line %d: Invalid conditional passed to FOR loop"%(p.lineno(3))))
-
-    def p_loop_error_b(self, p):
-        ''' loop : FOREACH var-lhs LPAREN error RPAREN codeblock continue
-        '''
-        self.error_list.append((p.lineno(4), "Line %d: Invalid conditional passed to FOREACH loop"%(p.lineno(4))))
-
-    def p_loop_error_c(self, p):
-        ''' loop : DO codeblock WHILE LPAREN error RPAREN SEMICOLON
-        '''
-        self.error_list.append((p.lineno(5), "Line %d: Invalid conditional passed to DO-WHILE loop"%(p.lineno(5))))
-    """
 
     def p_loop_label(self, p):
         ''' loop-label : ID COLON
@@ -1512,8 +1542,17 @@ class Parser(object):
 
             p[0].code = p[1].code | p[4].code | p[5].code | p[6].code | p[9].code | IR.GenCode("type, %s, %s"%(p[5].typePlace, p[9].typePlace)) | IR.GenCode("=, %s, %s"%(p[5].place, p[9].place))
 
+    def p_ternary_operator_error1(self, p):
+        ''' ternary-op : error TERNARY_CONDOP MARK-backpatch usable-expression MARK-ternary-assignment1 MARK-backpatch-nextlist COLON MARK-backpatch usable-expression 
+        '''
+        self.error_list.append((p.lineno(1), "Line %d: Invalid Conditional given to Ternary Operator (?:)"%(p.lineno(1))))
+
+    def p_ternary_operator_error2(self, p):
+        ''' ternary-op : boolean-expression TERNARY_CONDOP MARK-backpatch error MARK-ternary-assignment1 MARK-backpatch-nextlist COLON MARK-backpatch usable-expression 
+        '''
+        self.error_list.append((p.lineno(4), "Line %d: Expression (under then) given to Ternary Operator (?:) is incorrect"%(p.lineno(4))))
+
     # Build the parser
-    # error_seen decides if we should print the .html file or not at the end
     def build(self, **kwargs):
         self.parser = yacc.yacc(module=self, **kwargs)
         self.error_seen = False
